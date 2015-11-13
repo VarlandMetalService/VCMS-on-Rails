6314951abbfbc4bcb36d5ed6f67b26375db64331
Toby Varland <toby.varland@varland.com>
2015-11-13 09:02:13 -0500

    Fix Printing Format

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -3,6 +3,7 @@ class Permission < ActiveRecord::Base
   # Associations.
   has_many      :assigned_permissions
   has_many      :users,
+                -> { select('users.*, assigned_permissions.value AS access_level') },
                 :through => :assigned_permissions
   
 end
