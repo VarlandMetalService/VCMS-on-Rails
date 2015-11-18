a21a69f1ac64d59cd3e744deb56437524a419697
Toby Varland <toby.varland@varland.com>
2015-11-18 10:53:57 -0500

    Change Sort Order

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -5,7 +5,7 @@ class Permission < ActiveRecord::Base
 
   # Associations.
   has_many      :assigned_permissions,
-                -> { includes(:user).order('users.employee_number DESC') }
+                -> { includes(:user).order('users.employee_number ASC') }
   has_many      :users,
                 -> { select('users.*, assigned_permissions.value AS access_level') },
                 :through => :assigned_permissions
